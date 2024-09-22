c8d6cab4f33fef2511819772494c791deaa61606
Volodrum <volodimirshakula@gmail.com>
2024-09-22 11:05:08 +0300

    Add sharedmodels library with classes
    User, Product, Order

---
+++ AspMicroservices/SharedModels/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedModels
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
