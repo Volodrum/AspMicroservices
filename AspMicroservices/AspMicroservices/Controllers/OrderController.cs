2cf52cf2b267095e5c2a6ecd485cba5a47b0594c
Volodrum <volodimirshakula@gmail.com>
2024-09-22 11:32:11 +0300

    add order controller

---
+++ AspMicroservices/AspMicroservices/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedModels;
+using System.Text.Json;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _userServiceUrl = "https://localhost:7005/api/user";
+        private readonly string _productServiceUrl = "https://localhost:7214/api/product";
+
+        private static readonly List<Order> Orders = new List<Order>();
+        public OrderController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder([FromBody] Order order)
+        {
+            var user = await GetUserById(order.UserId);
+            if (user == null)
+                return BadRequest("User not found");
+
+            var product = await GetProductById(order.ProductId);
+            if (product == null)
+                return BadRequest("Product not found");
+
+            Orders.Add(order);
+            return Ok(order);
+        }
+
+        private async Task<User?> GetUserById(int userId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"{_userServiceUrl}/{userId}");
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<User>(json);
+        }
+
+        private async Task<Product?> GetProductById(int productId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"{_productServiceUrl}/{productId}");
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Product>(json);
+        }
+    }
+}
